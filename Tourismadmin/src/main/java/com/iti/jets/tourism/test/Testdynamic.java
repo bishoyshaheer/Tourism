812442b9321a22962d7af569ae0926e04adaa789
marwaibraheem <marwaibraheem99@gmail.com>
2015-05-02 13:58:04 +0200

    ADD Servlet Class To Get The Image , Add Test Class To Create Componenet Dynamically

--- Tourismadmin/src/main/java/com/iti/jets/tourism/test/Testdynamic.java
+++ Tourismadmin/src/main/java/com/iti/jets/tourism/test/Testdynamic.java
@@ -1,81 +1,108 @@
-//package com.iti.jets.tourism.test;
-//import java.util.ArrayList;
-//
-//import java.util.Collection;
-//import java.util.List;
-//
-//
-//import javax.el.ELContext;
-//import javax.el.ExpressionFactory;
-//import javax.faces.application.Application;
-//import javax.faces.bean.SessionScoped;
-//import javax.faces.component.html.HtmlForm;
-//import javax.faces.component.html.HtmlInputText;
-//import javax.faces.component.html.HtmlOutputText;
-//import javax.faces.component.html.HtmlPanelGrid;
-//import javax.faces.context.FacesContext;
-//
-//import static com.sun.faces.el.ELUtils.createValueExpression;
-//
-///**
-// * Created by AYA on 01/05/2015.
-// */
-//
-//
-//@javax.faces.bean.ManagedBean(name = "backing_testdynamic")
-//@SessionScoped
-//public class Testdynamic {private HtmlForm form1;
-//    private Collection cl;
-//    private HtmlPanelGrid gridComponent;
-// private String data;
-//List<String> test=new ArrayList<>();
-//    public  Testdynamic() {
-//        cl=new ArrayList();
-//  int  j=0;
-//
-//            HtmlInputText inputPutText = new HtmlInputText();
-//            gridComponent=new HtmlPanelGrid();
-//            inputPutText.setId("iptextCol1-"+j);
-//            cl.add(inputPutText);
-//
-//        gridComponent.getChildren().addAll(cl);
-//
-//
-//    }
-//
-//    public void addFunction(){
-//        int i=gridComponent.getChildCount();
-//        HtmlInputText inputPutText1 = new HtmlInputText();
-//        inputPutText1.setId("iptextcol1-" + i++);
-//        FacesContext fctx = FacesContext.getCurrentInstance();
-//        ELContext elctx = fctx.getELContext();
-//        Application jsfApp = fctx.getApplication();
-//        ExpressionFactory exprFactory = jsfApp.getExpressionFactory();
-//        inputPutText1.setValueExpression("value", createValueExpression("#{backing_testdynamic.data}"));
-//        gridComponent.getChildren().add(inputPutText1);
-//
-//
-//    }
-//
-//    public String getData() {
-//        return data;
-//    }
-//
-//    public void setData(String data) {
-//        this.data = data;
-//    }
-//
-//    public void view(){
+package com.iti.jets.tourism.test;
+import java.util.ArrayList;
+
+import java.util.Collection;
+import java.util.List;
+
+
+import javax.el.ELContext;
+import javax.el.ExpressionFactory;
+import javax.el.ValueExpression;
+import javax.faces.application.Application;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.html.HtmlForm;
+import javax.faces.component.html.HtmlInputText;
+import javax.faces.component.html.HtmlOutputText;
+import javax.faces.component.html.HtmlPanelGrid;
+import javax.faces.context.FacesContext;
+
+import static com.sun.faces.el.ELUtils.createValueExpression;
+
+/**
+ * Created by AYA on 01/05/2015.
+ */
+
+
+@javax.faces.bean.ManagedBean(name = "backing_testdynamic")
+@SessionScoped
+public class Testdynamic {private HtmlForm form1;
+    private Collection cl;
+    private HtmlPanelGrid gridComponent;
+ private String data;
+String [] test=new String[10];
+    public  Testdynamic() {
+        cl=new ArrayList();
+  int  j=0;
+
+            HtmlInputText inputPutText = new HtmlInputText();
+            gridComponent=new HtmlPanelGrid();
+            inputPutText.setId("iptextCol1-"+j);
+            cl.add(inputPutText);
+        inputPutText.setValueExpression("value",
+                resolveExpression("#{backing_testdynamic.test['" + 0 + "']}"));
+        gridComponent.getChildren().addAll(cl);
+
+
+    }
+
+    public void addFunction(){
+        int i=gridComponent.getChildCount();
+     //   System.out.println(i);
+        HtmlInputText inputPutText1 = new HtmlInputText();
+        inputPutText1.setId("iptextcol1-" + i++);
+        FacesContext fctx = FacesContext.getCurrentInstance();
+        ELContext elctx = fctx.getELContext();
+        Application jsfApp = fctx.getApplication();
+        ExpressionFactory exprFactory = jsfApp.getExpressionFactory();
+      //  inputPutText1.setValueExpression("value", createValueExpression("#{backing_testdynamic.data}"));
+
+        inputPutText1.setValueExpression("value",
+                resolveExpression("#{backing_testdynamic.test['" + i+ "']}"));
+        gridComponent.getChildren().add(inputPutText1);
+
+
+    }
+
+    public static ValueExpression resolveExpression(String expression) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        Application app = facesContext.getApplication();
+        ExpressionFactory elFactory = app.getExpressionFactory();
+        ELContext elContext = facesContext.getELContext();
+        ValueExpression valueExp = elFactory.createValueExpression(elContext,
+                expression, String.class);
+        return valueExp;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String[] getTest() {
+        return test;
+    }
+
+    public void setTest(String[] test) {
+        this.test = test;
+    }
+
+    public void view(){
 //        System.out.println(data);
-//    }
-//
-//    public void setGridComponent(HtmlPanelGrid gridComponent) {
-//        this.gridComponent = gridComponent;
-//    }
-//
-//    public HtmlPanelGrid getGridComponent() {
-//        return gridComponent;
-//    }
-//
-//
-//}
+        for (int i = 0; i <test.length ; i++) {
+            System.out.println(test[i]);
+        }
+    }
+
+    public void setGridComponent(HtmlPanelGrid gridComponent) {
+        this.gridComponent = gridComponent;
+    }
+
+    public HtmlPanelGrid getGridComponent() {
+        return gridComponent;
+    }
+
+
+}
